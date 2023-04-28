af541b057b640d9d78b54ad34fad142830d20d22
JieGG <947295340@qq.com>
2023-04-28 18:53:09 +0800

    更换使用 WebView2 浏览器核心

---
+++ AutomaticWebBrowser/Domain/Log/ILoggerFormatter.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Core;
+using Serilog.Formatting;
+
+namespace AutomaticWebBrowser.Domain.Log
+{
+    /// <summary>
+    /// 日志格式化接口
+    /// </summary>
+    interface ILoggerFormatter : ILogEventSink
+    {
+        ITextFormatter? TextFormatter { get; set; }
+    }
+}
---
+++ AutomaticWebBrowser/Domain/Log/LoggerHostScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices;
+
+using Serilog.Core;
+
+namespace AutomaticWebBrowser.Domain.Log
+{
+    [ComVisible (true)]
+    public class LoggerHostScript
+    {
+        #region --字段--
+        private readonly Logger logger;
+        #endregion
+
+        #region --构造函数--
+        public LoggerHostScript (Logger logger)
+        {
+            this.logger = logger;
+        }
+        #endregion
+
+        #region --公开方法--
+        public void Debug (string message)
+        {
+            this.logger.Debug (message);
+        }
+
+        public void Info (string message)
+        {
+            this.logger.Information (message);
+        }
+
+        public void Warning (string message)
+        {
+            this.logger.Warning (message);
+        }
+
+        public void Error (string message)
+        {
+            this.logger.Error (message);
+        }
+        #endregion
+    }
+}
